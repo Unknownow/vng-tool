84429bc4b6c4587ed1fa965651e11faa080a0010
cuongbpt82 <cuong.bpt82@gmail.com>
2021-06-18 22:58:44 +0700

    added regex to get tag and value

--- localize-tool/localize-gen-tool.js
+++ localize-tool/localize-gen-tool.js
@@ -38,7 +38,7 @@ for (let cellID in originalSheet) {
     keyColumnList.forEach(column => {
         const cellID = column + row;
         const value = originalSheet[cellID] ? originalSheet[cellID] : { h: keyString };
-        newFile[column][keyString] = value.h;
+        newFile[column][keyString] = value;
     });
 }
 
@@ -56,3 +56,35 @@ let outputFile = fs.openSync("localize-tool/test.json", "w+");
 fs.writeFileSync(outputFile, JSON.stringify(newFile), "utf8");
 fs.closeSync(outputFile);
 
+/*
+    <span style=\"font-size:13pt;\">Vui</span>
+    <span style=\"font-size:13pt;\"></span>
+    <span style=\"font-size:18pt;\">Cùng</span>
+    <span style=\"font-size:13pt;\"></span>
+    <span style=\"font-size:13pt;\">
+        <b>Euro</b>
+    </span>
+    <span style=\"font-size:13pt;\"></span>
+    <span style=\"font-size:13pt;\">
+        <i>2020</i>
+    </span>
+*/
+const regexComponent = RegExp(/<(.*)\b[^>]*.*?<\/\1>/g);
+const regexTag = RegExp(/(?<=<)[^<>/]+?(?=>)/g);
+const regexValue = RegExp(/(?<=>)[^<>/]+?(?=<)/g);
+
+/**
+ * 
+ * @param {string} value 
+ */
+const formatValue = (value) => {
+    const subValue = value.match(regexComponent);
+    subValue.forEach(value => {
+        console.log(JSON.stringify(value))
+        console.log(JSON.stringify(value.match(regexTag)[0]))
+        console.log("\n")
+    });
+    let finalString = "";
+}
+
+formatValue("<span style=\"font-size:13pt;\">Vui</span><span style=\"font-size:13pt;\"> </span><span style=\"font-size:18pt;\">Cùng</span><span style=\"font-size:13pt;\"> </span><span style=\"font-size:13pt;\"><b>Euro</b></span><span style=\"font-size:13pt;\"> </span><span style=\"font-size:13pt;\"><i>2020</i></span>");
---
+++ localize-tool/tag.js
@@ -0,0 +1,5 @@
+const availableTag = {
+    font: "<font = @value>@content</font>",
+    size: "<size = @value>@content</size>",
+    color: "<color = @value>@content</color>",
+};
